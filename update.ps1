73708464294f48f37d94fbaacb535a35f90604c4
Ruslan <ruslan@users.noreply.github.com>
2025-12-25 03:35:35 +0200

    Auto update 2025-12-25 03:35

--- update.ps1
+++ update.ps1
@@ -1,5 +1,13 @@
-git status
+cd "$(Split-Path -Parent $MyInvocation.MyCommand.Path)"
+
+if (-not (git status --porcelain)) {
+    Write-Host "No changes to commit"
+    exit
+}
+
 git add .
+
 $time = Get-Date -Format "yyyy-MM-dd HH:mm"
 git commit -m "Auto update $time"
+
 git push
